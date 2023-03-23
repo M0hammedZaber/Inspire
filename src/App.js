22aa4221f25ba6862cd809c5810d036bb7860d15
Mo <mohammedzaber@hotmail.com>
2023-03-23 14:33:16 +0000

    added event listener to button so when clicked a new quote is generated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-import React from 'react';
 import './App.css';
-import '../src/pages/home.css';
-// var assert = require('assert');
-// var quoteScraper = require('./');
-
-// var quoteScraper = require('quote-scraper');
-
-// // quote-scraper gives you an array with famous quote from someone.
-// // Just give the name (case insensitive)
-// // and you will get quotes in english.
-// console.log(quoteScraper('Agatha Christie'));
-
-// console.log('test for es');
-// quoteScraper('mark twain', 'es');
 
 // inspirational NPM that works and returns any random quote
 
@@ -23,18 +9,3 @@ function App() {
 }
 
 export default App
-
-// motivational quote NPM that works in console log
-
-// const quotes = require("success-motivational-quotes");
-
-// console.log(quotes.getTodaysQuote());
-
-// attempting to have motivational quote NPM not in console log 
-
-// const getTodaysQuote = () => {
-//   const length = data.length;
-//   const number = Math.floor(Math.random() * length);
-//   const todaysQuote = data[number];
-//   return todaysQuote;
-// };
