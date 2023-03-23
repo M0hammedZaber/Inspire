d6561f1431c670b205e49d0f4d537f7712115c9f
Mo <mohammedzaber@hotmail.com>
2023-03-23 11:16:37 +0000

    testing different NPMs

--- src/App.js
+++ src/App.js
@@ -14,12 +14,27 @@ import './App.css';
 // console.log('test for es');
 // quoteScraper('mark twain', 'es');
 
-const Quote = require('inspirational-quotes');
+// inspirational NPM that works and returns any random quote
 
-// returns any random quote
+const Quote = require('inspirational-quotes');
 
 function App() {
   return(Quote.getRandomQuote());
 }
 
 export default App
+
+// motivational quote NPM that works in console log
+
+// const quotes = require("success-motivational-quotes");
+
+// console.log(quotes.getTodaysQuote());
+
+// attempting to have motivational quote NPM not in console log 
+
+// const getTodaysQuote = () => {
+//   const length = data.length;
+//   const number = Math.floor(Math.random() * length);
+//   const todaysQuote = data[number];
+//   return todaysQuote;
+// };
